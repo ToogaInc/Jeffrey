2b58ff24b682e41adbf12d2b1fd2e6cf4cd8c69f
Dylan Stauch <dylan@Dylans-MacBook-Pro.local>
2023-08-20 14:15:59 -1000

    Added requested changes

--- src/commands/mock.ts
+++ src/commands/mock.ts
@@ -2,50 +2,50 @@ import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
 import { mockTargets } from '../index';
 
 export const Mock = {
-  info: new SlashCommandBuilder()
-    .setName('mock')
-    .setDescription("Mocks designated member")
-    .addUserOption(option =>
-        option.setName('member')
-            .setDescription('Choose which member you want to be mocked')
-            .setRequired(true)),
-    
+    info: new SlashCommandBuilder()
+        .setName('mock')
+        .setDescription('Mocks designated member')
+        .addUserOption(option =>
+            option.setName('member')
+                .setDescription('Choose which member you want to be mocked')
+                .setRequired(true)),
+
     run: async (interaction: CommandInteraction): Promise<void> => {
-        if (!interaction.guild){ //checks if the command is NOT done in a server
-            await interaction.reply(`This command can only be done in a server.`);
+        if (!interaction.guild) { //Checks if the command is NOT done in a server
+            await interaction.reply('This command can only be done in a server.');
             return;
         }
-            //Looks for "staff" role in server      
-          const role = interaction.guild.roles.cache.find(role => role.name === "staff");
+        //Looks for 'staff' role in server (case insensitive)     
+        const role = interaction.guild.roles.cache.find(role => role.name.toLowerCase() === 'staff');
 
-          if (!role) { 
-            await interaction.reply(`There is no "staff" role on this server. + 
-                                     Contact a developer for more information.`);
+        if (!role) {
+            await interaction.reply('There is no \"staff\" role on this server.' + 
+                                     ' Contact a developer for more information.');
             return;
-          }
-            //Finds the users ID, then checks if the user has a valid role
-          const commandUser = await interaction.guild.members.fetch(interaction.user.id);
-
-          if (!commandUser.roles.cache.has(role.id)) {
-            await interaction.reply(`You do not have permission to use this command.`);
-            return;
-          }
-          
-          const target = interaction.options.getUser('member');
-
-          if(!target){
-            await interaction.reply(`Cannot find specified member`);
-            return;
-          }
-          if (target.id === commandUser.id){
-            await interaction.reply(`You can\'t mock yourself!`);
-            return;
-          }
-          if (target.id === interaction.client.user.id){
-            await interaction.reply(`Please don\'t mock me UwU.`);
-            return;
-          }
-            mockTargets.add(target.id);
-            await interaction.reply(`Sure thing, I will begin mocking ${target}!`);
         }
-    };
+        //Finds the users ID, then checks if the user has a valid role
+        const commandUser = await interaction.guild.members.fetch(interaction.user.id);
+
+        if (!commandUser.roles.cache.has(role.id)) {
+            await interaction.reply('You do not have permission to use this command.');
+            return;
+        }
+
+        const target = interaction.options.getUser('member');
+
+        if (!target) {
+            await interaction.reply('Cannot find specified member');
+            return;
+        }
+        if (target.id === commandUser.id) {
+            await interaction.reply('You can\'t mock yourself!');
+            return;
+        }
+        if (target.id === interaction.client.user.id) {
+            await interaction.reply('Please don\'t mock me UwU.');
+            return;
+        }
+        mockTargets.add(target.id);
+        await interaction.reply(`Sure thing, I will begin mocking ${target}!`);
+    }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 // main.js
 import { config } from 'dotenv';
-import { 
-  Client, 
-  ClientOptions, 
-  REST } from 'discord.js';
+import {
+  Client,
+  ClientOptions,
+  REST
+} from 'discord.js';
 import { Ping } from './commands/ping';
 import { Mock } from './commands/mock';
 import { Routes, GatewayIntentBits } from 'discord-api-types/v9';
@@ -37,21 +38,26 @@ const rest = new REST({ version: '10' }).setToken(token!);
 
 async function main() {
   try {
-    console.log('Started refreshing application (/) commands.');
+      console.log('Started refreshing application (/) commands.');
 
-    await rest.put(Routes.applicationGuildCommands(clientID!, guildID!), { body: [
-      Ping.info.toJSON(),
-      Mock.info.toJSON()
-      ] 
-    });
+      await rest.put(
+          Routes.applicationGuildCommands(clientID!, guildID!),
+          {
+              body: [
+                  Ping.info.toJSON(),
+                  Mock.info.toJSON()
+              ]
+          }
+      );
 
-    console.log('Successfully reloaded application (/) commands.');
+      console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
-    console.error(error);
+      console.error(error);
   }
 }
 
-export let mockTargets = new Set();
+
+export const mockTargets = new Set();
 
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) return;
