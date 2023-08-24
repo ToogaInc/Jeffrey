c70138b5787f4d133976c6f7ac270cf7673188ad
Dylan <dylanstauch@gmail.com>
2023-08-23 15:28:00 -1000

    Global Rate Limit for all commands

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,9 @@ import { Cat } from './commands/cat';
 
 config();
 
+const cooldown = new Map<string, Map<string, number>>();
+const cooldownTime = 5000;
+
 export const mockTargets = new Set();
 
 const token = process.env.BOT_TOKEN;
@@ -62,11 +65,32 @@ async function main() {
   }
 }
 
-
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) return;
 
   const { commandName } = interaction;
+  const userID = interaction.user.id;
+
+  if (!cooldown.has(commandName)) {
+    cooldown.set(commandName, new Map<string, number>());
+  }
+  const cooldownMap = cooldown.get(commandName)!;
+
+  if (cooldownMap.has(userID) && cooldownMap.get(userID)! > Date.now()) {
+    const cooldownRemaining = (cooldownMap.get(userID)! - Date.now()) / 1000;
+    await interaction.reply(`Please wait ${cooldownRemaining.toFixed(1)} seconds.`);
+    return;
+  }
+  cooldownMap.set(userID, Date.now() + cooldownTime);
+
+  for (const [commandName, cooldownMap] of cooldown) {
+    if (cooldownMap.has(userID) && cooldownMap.get(userID)! <= Date.now()) {
+      cooldownMap.delete(userID);
+    }
+  }
+
+  console.log(`user ${interaction.user.username} (${userID}) ran the '${commandName}' command | Guild: ${interaction.guild} |`
+    + ` Channel: ${interaction.channel} | Timestamp: ${interaction.createdAt}`);
 
   if (commandName === 'ping') {
     await Ping.run(interaction);
