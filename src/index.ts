ab0b66b39defa8fbb734e05def2065000aad4adc
Dylan <dylanstauch@gmail.com>
2023-08-24 18:34:32 -1000

    Remove un-necessary code related to global rate limit

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,20 @@ import { config } from 'dotenv';
 import {
   Client,
   ClientOptions,
-  REST
+  REST,
+  Routes,
+  GatewayIntentBits
 } from 'discord.js';
 import { Ping } from './commands/ping';
 import { Mock } from './commands/mock';
-import { Routes, GatewayIntentBits } from 'discord-api-types/v9';
-
 
 
 config();
+
+const cooldown = new Map<string, Map<string, number>>();
+const cooldownTime = 5000;
+
+export const mockTargets = new Set();
 
 const token = process.env.BOT_TOKEN;
 const clientID = process.env.CLIENT_ID;
@@ -19,8 +24,10 @@ const guildID = process.env.GUILD_ID;
 
 const intents = [
   GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMembers,
   GatewayIntentBits.GuildMessages,
-];
+  GatewayIntentBits.MessageContent,
+]
 
 const options: ClientOptions = {
   intents: intents,
@@ -38,31 +45,44 @@ const rest = new REST({ version: '10' }).setToken(token!);
 
 async function main() {
   try {
-      console.log('Started refreshing application (/) commands.');
+    console.log('Started refreshing application (/) commands.');
 
-      await rest.put(
-          Routes.applicationGuildCommands(clientID!, guildID!),
-          {
-              body: [
-                  Ping.info.toJSON(),
-                  Mock.info.toJSON()
-              ]
-          }
-      );
+    await rest.put(
+      Routes.applicationGuildCommands(clientID!, guildID!),
+      {
+        body: [
+          Ping.info.toJSON(),
+          Mock.info.toJSON()
+        ]
+      }
+    );
 
-      console.log('Successfully reloaded application (/) commands.');
+    console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
-      console.error(error);
+    console.error(error);
   }
 }
-
-
-export const mockTargets = new Set();
 
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) return;
 
   const { commandName } = interaction;
+  const userID = interaction.user.id;
+
+  if (!cooldown.has(commandName)) {
+    cooldown.set(commandName, new Map<string, number>());
+  }
+  const cooldownMap = cooldown.get(commandName)!;
+
+  if (cooldownMap.has(userID) && cooldownMap.get(userID)! > Date.now()) {
+    const cooldownRemaining = (cooldownMap.get(userID)! - Date.now()) / 1000;
+    await interaction.reply(`Please wait ${cooldownRemaining.toFixed(1)} seconds.`);
+    return;
+  }
+  cooldownMap.set(userID, Date.now() + cooldownTime);
+
+  console.log(`user ${interaction.user.username} (${userID}) ran the '${commandName}' command | Guild: ${interaction.guild} |`
+    + ` Channel: ${interaction.channel} | Timestamp: ${interaction.createdAt}`);
 
   if (commandName === 'ping') {
     await Ping.run(interaction);
